3cf032b14ed3d6641df2182d489e41928547cf68
succi0303 <succi0303@gmail.com>
2023-11-19 22:29:28 +0900

    Add nvim/init.lua config file

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- encoding
+vim.o.encoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
+
+-- visual
+vim.o.ambiwidth = 'double'
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.visualbell = true
+vim.o.number = true
+vim.o.showmatch = true
+vim.o.matchtime = 1
+
+-- search
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohl<CR>', { noremap = true, silent = true})
