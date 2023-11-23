60af2a9f38105cb2b97d01f2b94cc3ca62c440a0
succi0303 <succi0303@gmail.com>
2023-11-23 12:35:23 +0900

    Install lazy.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,6 +14,9 @@ vim.o.visualbell = true
 vim.o.number = true
 vim.o.showmatch = true
 vim.o.matchtime = 1
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.list = true
 
 -- search
 vim.o.incsearch = true
@@ -21,3 +24,22 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.hlsearch = true
 vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohl<CR>', { noremap = true, silent = true})
+
+-- manipulation
+vim.g.mapleader = ','
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+plugins = require('plugins')
+require('lazy').setup(plugins)
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -12,5 +12,81 @@ return {
     config = function()
       require('lualine').setup()
     end
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    requires = { { 'nvim-lua/plenary.nvim' } },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader><leader>f', builtin.find_files)
+      vim.keymap.set('n', '<leader>ub', builtin.buffers)
+      vim.keymap.set('n', '<leader>uh', builtin.help_tags)
+      vim.keymap.set('n', '<leader>ug', builtin.live_grep)
+      }
+    end
+  },
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufRead",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        autotag = { enable = true },
+        highlight = {
+          enable = true,
+          -- disable = function(_, buf)
+          --   local max_filesize = 100 * 1024 -- 100 KB
+          --   local _ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          --   if _ok and stats and stats.size > max_filesize then
+          --     return true
+          --   end
+          -- end,
+        },
+        indent = { enable = true },
+        ensure_installed = {
+          "astro",
+          "bash",
+          "c",
+          "comment",
+          "cpp",
+          "css",
+          "dart",
+          "dockerfile",
+          "go",
+          "gomod",
+          "graphql",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          "jsonc",
+          "lua",
+          "make",
+          "markdown",
+          "php",
+          "prisma",
+          "python",
+          "regex",
+          "rust",
+          "scss",
+          "sparql",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
+      })
+    end,
   }
 }
